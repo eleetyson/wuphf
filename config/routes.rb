563b753ca47406e891881ac74e7faae37b2b3c51
Ethan <eleetyson@gmail.com>
2020-09-28 09:07:47 -0400

    build out login / sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :friends
-  resources :users
+  root 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  resources :users, only: [:new, :create]
+  # resources :messages
+  # resources :friends
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
