e25f2ba73472b3959a6c2f73e4480619f1c764db
Ethan <eleetyson@gmail.com>
2020-09-28 09:38:36 -0400

    add associations to models

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,4 @@
 class Friend < ApplicationRecord
+  has_many :messages
+  has_many :users, through: :messages
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   # resources :messages
   # resources :friends
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
