194fd30aadf6c24d72ac9f9f2f3af5c60e1cb30a
Ethan <eleetyson@gmail.com>
2020-09-29 08:54:46 -0400

    add helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+
+# finds the current user based on the value for the user_id key in the session hash
+# returns nil if user is not "logged in"
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+# returns true or false depending whether the session hash has a user_id key / value
+  def logged_in?
+    !!session[:user_id]
+  end
+
+# redirect to root if user isn't "logged in"
+  def require_login
+    redirect_to '/' unless logged_in?
+  end
+
+# redirect to ___ if user is "logged in"
+  def require_logout
+    # route tbd
+    # redirect_to '/home' if logged_in?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 # GET /login
   def new
-
   end
 
 # POST /login
@@ -12,11 +11,25 @@ class SessionsController < ApplicationController
 # GET /auth/google_oauth2/callback
 # separate login in for google oauth
   def omniauth
+    binding.pry
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
 
+    session[:user_id] = @user.id
+
+    # render 'welcome/home'
   end
 
 # POST /logout
   def destroy
 
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
