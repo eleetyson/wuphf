f66785b7617964d88d8a4d6da5a7037633bd146f
Ethan <eleetyson@gmail.com>
2020-09-29 09:09:39 -0400

    build out Google auth log in path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,26 +9,29 @@ class SessionsController < ApplicationController
   end
 
 # GET /auth/google_oauth2/callback
-# separate login in for google oauth
+# log the user in if they authed in with Google before
+# sign them up if not
+# redirect to dashboard
   def omniauth
-    binding.pry
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
+      u.password = SecureRandom.hex(10)
     end
 
     session[:user_id] = @user.id
-
     # render 'welcome/home'
   end
 
 # POST /logout
+# logs the user out and redirects them to the root page
   def destroy
-
+    session.delete(:user_id)
+    redirect_to root_path
   end
 
   private
-
+# helper method to make parsing omniauth response simpler
   def auth
     request.env['omniauth.auth']
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :friends, through: :messages
   has_secure_password
+  validates :name, presence: true
+  validates :uid, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
