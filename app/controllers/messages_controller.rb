7cc813d4dab68f370f20e23ca2a71888bb6945af
Ethan <eleetyson@gmail.com>
2020-09-29 21:52:01 -0400

    build out messages#create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,22 @@ class MessagesController < ApplicationController
 # POST /messages
 # to add logic handling input from form for new friend
 # or simpler input from form for msg to existing friend
+
+# message[content]
+# message[friend][name]
+# message[friend][email]
+# message[friend][phone]
   def create
-    if params[:message][:friend][:name]
-      "this"
+    if params[:message][:friend]
+      @message = Message.new(content: params[:message][:content])
+      @message.user = current_user
+      f = Friend.new(name: params[:message][:friend][:name], email: params[:message][:friend][:email], phone: params[:message][:friend][:phone])
+      if f.save
+        @message.friend = f
+      else
+        "ds"
+        # error message
+      end
     else
       @message = Message.new(message_params)
     end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :friend
+  validates :content, presence: true
 end
