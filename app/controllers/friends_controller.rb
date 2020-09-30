17df0dc55759a75079daae3d251b9df98d52d17a
Ethan <eleetyson@gmail.com>
2020-09-30 18:27:18 -0400

    build out new and edit friend forms

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,18 +1,27 @@
 class FriendsController < ApplicationController
   before_action :require_login
+  layout: "dashboard"
 
   def index
   end
 
+# GET /users/:id/friends/new
+# new_user_friend_path(current_user)
   def new
+    @friend = current_user.friends.build
   end
 
+# POST /users/:id/friends
   def create
   end
 
+# GET /users/:user_id/friends/:id/edit
+# edit_user_friend_path(current_user, the friend)
   def edit
   end
 
+# PATCH /users/:user_id/friends/:id
+# user_friend_path(current_user, the friend)
   def update
   end
 
