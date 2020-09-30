91b4e6087b48a100b2e1677261f33e24663c8f70
Ethan <eleetyson@gmail.com>
2020-09-30 15:19:38 -0400

    update scope method with a limit of 2 results

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -4,7 +4,7 @@ class Friend < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :phone, presence: true, length: { is: 10 }
-  # ActiveRecord scope method to return a user's recently added friends (ex: user.friends.recently_added)
+  # AR scope method to return a user's two most recently added friends (ex: user.friends.recently_added)
   # could be used at the class level in a feed displaying recent activity
-  scope :recently_added, -> { order(created_at: :desc) }
+  scope :recently_added, -> { order(created_at: :desc).limit(2) }
 end
