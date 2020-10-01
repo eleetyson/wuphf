022ca92756f43b71ac5b25921e7aefdb0c5e2b57
Ethan <eleetyson@gmail.com>
2020-10-01 11:06:11 -0400

    adjust environments with mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_options = {from: 'wuphf.noreply@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['USERNAME'],
+  password:             ENV['PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,22 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_options = {from: 'wuphf.noreply@gmail.com'}
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  # config.action_mailer.default_url_options = {host: 'heroku_url'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['USERNAME'],
+  password:             ENV['PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
 end
