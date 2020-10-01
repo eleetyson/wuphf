527b875333d8d143174915441e2b48d6faf7f57d
Ethan <eleetyson@gmail.com>
2020-10-01 10:27:40 -0400

    remove unneeded helper

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -5,14 +5,4 @@ module FriendsHelper
       button_to("Delete Friend", "/users/#{current_user.id}/friends/#{friend.id}", method: :delete, data: {confirm: "Are you sure you want to delete your friend #{friend.name}?"}, style: 'background-color: #cc3700;')
     end
   end
-
-# helper for the new / edit friend views to render submit button with different text for each
-  def submit_button_display(friend)
-    if friend.id
-      "Update Friend"
-    else
-      "Add Friend"
-    end
-  end
-
 end
