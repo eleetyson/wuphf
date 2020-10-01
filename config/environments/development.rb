929dbba19864710e68fde99986e0f285ebe36c57
Ethan <eleetyson@gmail.com>
2020-10-01 14:16:53 -0400

    update Google auth button and production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'wuphf.noreply@gmail.com'}
 
   config.action_mailer.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,9 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_options = {from: 'wuphf.noreply@gmail.com'}
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
-  # config.action_mailer.default_url_options = {host: 'heroku_url'}
+  config.action_mailer.default_url_options = {host: 'https://trywuphf.herokuapp.com/'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
