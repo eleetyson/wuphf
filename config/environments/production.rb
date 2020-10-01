4b195a797ddf8f8928955ad06198f0ec8867a62c
Ethan <eleetyson@gmail.com>
2020-10-01 14:51:25 -0400

    adjust production to display static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,10 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
