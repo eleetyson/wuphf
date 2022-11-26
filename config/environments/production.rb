a1f70c88f0aca0dd18927acf664137861147503d
Ethan <eleetyson@gmail.com>
2022-11-26 15:18:01 -0800

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   config.cache_classes = true
   config.serve_static_assets = true
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
